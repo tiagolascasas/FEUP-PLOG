4e69726e7c60a68b407b71b82106586ea97e3efb
tiagolascasas <up201503616@fe.up.pt>
2017-11-07 21:59:31 +0000

    New movement

--- proj1/oolong.pl
+++ proj1/oolong.pl
@@ -38,8 +38,8 @@ printPos(Table, Pos) :- pos(Table, Pos, X), write(X).
 
 printWaiterPos :- waiterPos(X, Y), write('Waiter in table '), write(X), write(' and position '), write(Y).
 
-printCurrPlayerMove(Pos) :- currentPiece(Player), write('Player '), write(Player), write(' placed a piece on table '),
-							waiterPos(Table, _), write(Table), write(', position '), write(Pos), nl.
+printCurrPlayerMove(Table, Pos) :- currentPiece(Player), write('Player '), write(Player), write(' placed a piece on table '),
+									write(Table), write(', position '), write(Pos), nl.
 
 printVictoryAnnouncement(Player) :- write('Player '), write(Player), write(' is victorious!'), nl.
 
@@ -51,15 +51,15 @@ printVictoryAnnouncement(Player) :- write('Player '), write(Player), write(' is 
 :- dynamic gameType/1.
 :- dynamic currentPiece/1.
 
-gameType('null').
+%gameType('null').
 
-start_1vs1 :- \+ gameType('1vs1'), \+ gameType('1vsAI'), \+ gameType('AIvsAI'), retract(gameType(null)),
+start_1vs1 :- \+ gameType('1vs1'), \+ gameType('1vsAI'), \+ gameType('AIvsAI'), %retract(gameType(null)),
 				assert(gameType('1vs1')), nl, initGame, write('1 vs 1 game started successfully'), nl, printBoard.
 
-start_1vsAI :- \+ gameType('1vs1'), \+ gameType('1vsAI'), \+ gameType('AIvsAI'), retract(gameType(null)),
+start_1vsAI :- \+ gameType('1vs1'), \+ gameType('1vsAI'), \+ gameType('AIvsAI'), %retract(gameType(null)),
 				assert(gameType('1vsAI')), nl, initGame, write('1 vs AI game started successfully'), nl, printBoard.
 
-start_AIvsAI :- \+ gameType('1vs1'), \+ gameType('1vsAI'), \+ gameType('AIvsAI'), retract(gameType(null)),
+start_AIvsAI :- \+ gameType('1vs1'), \+ gameType('1vsAI'), \+ gameType('AIvsAI'), %retract(gameType(null)),
 				assert(gameType('AIvsAI')), nl, initGame, write('AI vs AI game started successfully'), nl, printBoard.
 
 initGame :- assert(currentPiece(b)).
@@ -74,41 +74,65 @@ checkVictory :- checkVictoryPlayer(b) ; checkVictoryPlayer(g) ; fail.
 
 %checks if a player's pieces fulfill the victory conditions
 checkVictoryPlayer(Player) :- countTablePieces(Player, [n, s, e, w, nw, ne, sw, se, c], N), N >= 5,
-								printVictoryAnnouncement(Player).
+								printVictoryAnnouncement(Player), reset.
 
 %counts the number of tables in which a player has 5 or more pieces
 countTablePieces(_, [], 0).
 countTablePieces(Player, [Table|Lis], M) :- getPieceCountOnTable(Player, Table, Cnt),
 											Cnt >= 5, countTablePieces(Player, Lis, N), M is N + 1.
-countTablePieces(Player, [Table|Lis], N) :- countTablePieces(Player, Lis, N).
+countTablePieces(Player, [_|Lis], N) :- countTablePieces(Player, Lis, N).
 
 %gets the number of pieces of a certain player on a certain table
-getPieceCountOnTable(Player, Table, N) :- findall(X, pos(Table, _, Player), Lis), length(Lis, N).
+getPieceCountOnTable(Player, Table, N) :- findall(_, pos(Table, _, Player), Lis), length(Lis, N).
+
+%checks if a given table is full
+checkFullTable(Table) :- getPieceCountOnTable(o, Table, N), N == 0.
 
 %changes the active player
 flipCurrentPiece :- currentPiece(b) -> (retract(currentPiece(b)), assert(currentPiece(g))) ;
 										(retract(currentPiece(g)), assert(currentPiece(b))).
 
 %gets a move based on the game type
-getMove(X) :- gameType('AIvsAI'), !,getMoveAI(X).
-getMove(X) :- gameType('1vsAI'), !, getMove1vsAI(X).
-getMove(X) :- gameType('1vs1'), !, getMoveHuman(X).
+getMove(Table, Position) :- gameType('AIvsAI'), !,getMoveAI(Table, Position).
+getMove(Table, Position) :- gameType('1vsAI'), !, getMove1vsAI(Table, Position).
+getMove(Table, Position) :- gameType('1vs1'), !, getMoveHuman(Table, Position).
 
 %gets a move under the 1 vs AI mode: if the current piece is black,
 %gets it from the keyboard; otherwise, gets it from the AI
-getMove1vsAI(X) :- currentPiece(b) -> getMoveHuman(X) ; getMoveAI(X).
+getMove1vsAI(Table, Position) :- currentPiece(b) -> getMoveHuman(Table, Position) ; getMoveAI(Table, Position).
 
 %gets a move from the AI, testing for validity (randomizes a position until it is valid)
-getMoveAI(X) :- repeat,
-					random(0, 9, R), nth0(R, [n, s, e, w, nw, ne, sw, se, c], X),
-					(validPosition(X) -> ! ; fail).
+getMoveAI(Table, Position) :- waiterPos(T, _), checkFullTable(T), !,
+								repeat,
+									random(0, 9, R), nth0(R, [n, s, e, w, nw, ne, sw, se, c], Table),
+									(\+checkFullTable(Table) -> ! ; fail),
+								repeat,
+									random(0, 9, R), nth0(R, [n, s, e, w, nw, ne, sw, se, c], Position),
+									(\+validPosition(Position) -> ! ; fail).
+
+getMoveAI(Table, Position) :- repeat,
+								waiterPos(Table, _),
+								random(0, 9, R), nth0(R, [n, s, e, w, nw, ne, sw, se, c], Position),
+								(validPosition(Position) -> ! ; fail).
 
 %gets a move from the keyboard, testing for validity
-getMoveHuman(X) :- nl, write('Position of piece '), currentPiece(Piece), write(Piece), write(' '),
-					repeat,
-						read(X),
-						((validInput(X), validPosition(X)) -> !, (X == stop -> break ; !) ;
-								write('Invalid position, try again '), nl, fail).
+getMoveHuman(Table, Position) :- waiterPos(T, _), checkFullTable(T), !,
+									repeat,
+										read(Table),
+										((validInput(Table), \+checkFullTable(Table)) -> !, (Table == stop -> break ; !) ;
+												write('Invalid table, try again '), nl, fail),
+									repeat,
+										read(Position),
+										((validInput(Position), validPosition(Position)) -> !, (Position == stop -> break ; !) ;
+												write('Invalid position, try again '), nl, fail).
+
+
+getMoveHuman(Table, Position) :- nl, write('Position of piece '), currentPiece(Piece), write(Piece), write(' '),
+									waiterPos(Table, _),
+									repeat,
+										read(Position),
+										((validInput(Position), validPosition(Position)) -> !, (Position == stop -> break ; !) ;
+												write('Invalid position, try again '), nl, fail).
 
 %verifies if input is a valid table coord or the stop signal
 validInput(X) :- X == n ; X == s; X == e ; X== w ;
@@ -116,21 +140,21 @@ validInput(X) :- X == n ; X == s; X == e ; X== w ;
 					X == c ; X == stop.
 
 %verifies if the position within the waiter's table is valid
-validPosition(X) :- waiterPos(T, _),	%get waiter table
-					pos(T, X, o). 		%check if new position is vacant
+validPosition(Table, Position) :- pos(Table, Position, o).
+validPosition(stop, _).
+validPosition(_, stop).
 
 %moves the current piece to the position X of the waiter's table
-move(X) :- 	currentPiece(Piece),		%gets current piece (black or green)
-			waiterPos(T, P), 			%get current position of waiter
-			retract(pos(T, X, _)),		%clear new position for piece
-			assert(pos(T, X, Piece)),	%moves piece to new position
-			retract(waiterPos(T, P)), 	%removes current waiter pos
-			assert(waiterPos(X, T)),	%moves waiter to new pos
-			flipCurrentPiece.			%changes the current piece
+move(Table, Position) :-currentPiece(Piece),				%gets current piece (black or green)
+						retract(pos(Table, Position, _)),	%clear new position for piece
+						assert(pos(Table, Position, Piece)),%moves piece to new position
+						retract(waiterPos(_, _)), 			%removes current waiter pos
+						assert(waiterPos(Position, Table)),	%moves waiter to new pos
+						flipCurrentPiece.					%changes the current piece
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Start
+% Start and end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %starts a new game based on a given game type
@@ -149,6 +173,10 @@ startGame :-
 				fail
 			).
 
+reset :- retractall(pos(_,_,_)), retractall(gameType(_)), retractall(waiterPos(_,_)),
+		 retractall(currentPiece(_)), initPositions, assert(currentPiece(b)),
+		 write('Game finished successfully'), nl.
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Game cycle
@@ -157,12 +185,13 @@ startGame :-
 %main game loop. It starts a new match and repeats an input-logic-display cycle until
 %one of the players fulfills the victory conditions.
 play :- nl, write('Choose the type of game you want to play (1vs1/1vsAI/AIvsAI)'), nl,
-				startGame,							%initializes a match
+				initPositions,
+				startGame,										%initializes a match
 				repeat,
-						getMove(X),					%get move
-						printCurrPlayerMove(X),		%prints the current player's move
-						move(X),					%move piece
-						printBoard,					%show board
-						(checkVictory -> ! ; fail). %check victory (if fail, repeats)
+						getMove(Table, Position),				%get move
+						printCurrPlayerMove(Table, Position),	%prints the current player's move
+						move(Table, Position),					%move piece
+						printBoard,								%show board
+						(checkVictory -> ! ; fail). 		%check victory (if fail, repeats)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
--- proj1/positions.pl
+++ proj1/positions.pl
@@ -3,20 +3,37 @@
 :- dynamic pos/3.
 :- dynamic waiterPos/2.
 
-pos(nw, nw, o). pos(nw, n, o). pos(nw, ne, o).   	pos( n, nw, o). pos( n, n, o). pos( n, ne, o).   	pos(ne, nw, o). pos(ne, n, o). pos(ne, ne, o).
-pos(nw,  w, o). pos(nw, c, o). pos(nw, e,  o).   	pos( n,  w, o). pos( n, c, o). pos( n,  e, o).   	pos(ne,  w, o). pos(ne, c, o). pos(ne,  e, o).
-pos(nw, sw, o). pos(nw, s, o). pos(nw, se, o).   	pos( n, sw, o). pos( n, s, o). pos( n, se, o).   	pos(ne, sw, o). pos(ne, s, o). pos(ne, se, o).
+%pos(nw, nw, o), pos(nw, n, o), pos(nw, ne, o),   	pos( n, nw, o), pos( n, n, o), pos( n, ne, o),   	pos(ne, nw, o), pos(ne, n, o), pos(ne, ne, o),
+%pos(nw,  w, o), pos(nw, c, o), pos(nw, e,  o),   	pos( n,  w, o), pos( n, c, o), pos( n,  e, o),   	pos(ne,  w, o), pos(ne, c, o), pos(ne,  e, o),
+%pos(nw, sw, o), pos(nw, s, o), pos(nw, se, o),   	pos( n, sw, o), pos( n, s, o), pos( n, se, o),   	pos(ne, sw, o), pos(ne, s, o), pos(ne, se, o),
 
-pos( w, nw, o). pos( w, n, o). pos( w, ne, o).   	pos( c, nw, o). pos( c, n, o). pos( c, ne, o).   	pos( e, nw, o). pos( e, n, o). pos( e, ne, o).
-pos( w,  w, o). pos( w, c, o). pos( w, e,  o).   	pos( c,  w, o). pos( c, c, o). pos( c,  e, o).   	pos( e, w, o).  pos( e, c, o). pos( e,  e, o).
-pos( w, sw, o). pos( w, s, o). pos( w, se, o).   	pos( c, sw, o). pos( c, s, o). pos( c, se, o).   	pos( e, sw, o). pos( e, s, o). pos( e, se, o).
+%pos( w, nw, o), pos( w, n, o), pos( w, ne, o),   	pos( c, nw, o), pos( c, n, o), pos( c, ne, o),   	pos( e, nw, o), pos( e, n, o), pos( e, ne, o),
+%pos( w,  w, o), pos( w, c, o), pos( w, e,  o),   	pos( c,  w, o), pos( c, c, o), pos( c,  e, o),   	pos( e, w, o),  pos( e, c, o), pos( e,  e, o),
+%pos( w, sw, o), pos( w, s, o), pos( w, se, o),   	pos( c, sw, o), pos( c, s, o), pos( c, se, o),   	pos( e, sw, o), pos( e, s, o), pos( e, se, o),
 
-pos(sw, nw, o). pos(sw, n, o). pos(sw, ne, o).  	pos( s, nw, o). pos( s, n, o). pos( s, ne, o).   	pos(se, nw, o). pos(se, n, o). pos(se, ne, o).
-pos(sw,  w, o). pos(sw, c, o). pos(sw, e,  o).   	pos( s,  w, o). pos( s, c, o). pos( s,  e, o).   	pos(se, w, o).  pos(se, c, o). pos(se,  e, o).
-pos(sw, sw, o). pos(sw, s, o). pos(sw, se, o).   	pos( s, sw, o). pos( s, s, o). pos( s, se, o).   	pos(se, sw, o). pos(se, s, o). pos(se, se, o).
+%pos(sw, nw, o), pos(sw, n, o), pos(sw, ne, o),  	pos( s, nw, o), pos( s, n, o), pos( s, ne, o),   	pos(se, nw, o), pos(se, n, o), pos(se, ne, o),
+%pos(sw,  w, o), pos(sw, c, o), pos(sw, e,  o),   	pos( s,  w, o), pos( s, c, o), pos( s,  e, o),   	pos(se, w, o),  pos(se, c, o), pos(se,  e, o),
+%pos(sw, sw, o), pos(sw, s, o), pos(sw, se, o),   	pos( s, sw, o), pos( s, s, o), pos( s, se, o),   	pos(se, sw, o), pos(se, s, o), pos(se, se, o),
 
-waiterPos(c, c).
-pos(null, null, b).
-pos(null, null, g).
+%waiterPos(c, c),
+%pos(null, null, b),
+%pos(null, null, g),
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+initPositions :-
+assert(pos(nw, nw, o)), assert(pos(nw, n, o)), assert(pos(nw, ne, o)),   	assert(pos( n, nw, o)), assert(pos( n, n, o)), assert(pos( n, ne, o)),   	assert(pos(ne, nw, o)), assert(pos(ne, n, o)), assert(pos(ne, ne, o)),
+assert(pos(nw,  w, o)), assert(pos(nw, c, o)), assert(pos(nw, e,  o)),   	assert(pos( n,  w, o)), assert(pos( n, c, o)), assert(pos( n,  e, o)),   	assert(pos(ne,  w, o)), assert(pos(ne, c, o)), assert(pos(ne,  e, o)),
+assert(pos(nw, sw, o)), assert(pos(nw, s, o)), assert(pos(nw, se, o)),   	assert(pos( n, sw, o)), assert(pos( n, s, o)), assert(pos( n, se, o)),   	assert(pos(ne, sw, o)), assert(pos(ne, s, o)), assert(pos(ne, se, o)),
+
+assert(pos( w, nw, o)), assert(pos( w, n, o)), assert(pos( w, ne, o)),   	assert(pos( c, nw, o)), assert(pos( c, n, o)), assert(pos( c, ne, o)),   	assert(pos( e, nw, o)), assert(pos( e, n, o)), assert(pos( e, ne, o)),
+assert(pos( w,  w, o)), assert(pos( w, c, o)), assert(pos( w, e,  o)),   	assert(pos( c,  w, o)), assert(pos( c, c, o)), assert(pos( c,  e, o)),   	assert(pos( e, w, o)),  assert(pos( e, c, o)), assert(pos( e,  e, o)),
+assert(pos( w, sw, o)), assert(pos( w, s, o)), assert(pos( w, se, o)),   	assert(pos( c, sw, o)), assert(pos( c, s, o)), assert(pos( c, se, o)),   	assert(pos( e, sw, o)), assert(pos( e, s, o)), assert(pos( e, se, o)),
+
+assert(pos(sw, nw, o)), assert(pos(sw, n, o)), assert(pos(sw, ne, o)),  	assert(pos( s, nw, o)), assert(pos( s, n, o)), assert(pos( s, ne, o)),   	assert(pos(se, nw, o)), assert(pos(se, n, o)), assert(pos(se, ne, o)),
+assert(pos(sw,  w, o)), assert(pos(sw, c, o)), assert(pos(sw, e,  o)),   	assert(pos( s,  w, o)), assert(pos( s, c, o)), assert(pos( s,  e, o)),   	assert(pos(se, w, o)),  assert(pos(se, c, o)), assert(pos(se,  e, o)),
+assert(pos(sw, sw, o)), assert(pos(sw, s, o)), assert(pos(sw, se, o)),   	assert(pos( s, sw, o)), assert(pos( s, s, o)), assert(pos( s, se, o)),   	assert(pos(se, sw, o)), assert(pos(se, s, o)), assert(pos(se, se, o)),
+
+assert(waiterPos(c, c)),
+assert(pos(null, null, b)),
+assert(pos(null, null, g)).
