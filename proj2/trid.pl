484e8d5b930271c160035e36c29036c38e567694
tiagolascasas <up201503616@fe.up.pt>
2017-12-17 21:29:00 +0000

    Left diagonals

--- proj2/trid.pl
+++ proj2/trid.pl
@@ -11,7 +11,7 @@ trid(N, R) :-
 
 testSolver :-
         V = [[V1],[V2, V3],[V4, V5, V6],[V7, V8, V9, V10],[V11, V12, V13, V14, V15]],
-        T = [[V2, V4, V5, 12],[V4, V7, V8, 9],[V7, V11, V12, 6],[V7, V11, V12, 6],[V8,V12,V13,12],[V9,V13,V14,8]],
+        T = [[V2, V4, V5, 12],[V4, V7, V8, 9],[V7, V11, V12, 6],[V7, V11, V12, 6],[V8, V12, V13, 12],[V9, V13, V14, 8]],
         R = 5,
         solveTrid(V, T, R),
         write(V).
@@ -27,38 +27,64 @@ testSolver :-
 solveTrid(V, T, R) :-
 		setDomain(V, R),
 		horizontal(V),
-		%diagonalsRight(V),
-		%diagonalsLeft(V),
+        diagonalsRight(V),
+		diagonalsLeft(V),
 		innerSums(T),
 		flatten(V, Vf),
 		labeling([], Vf).
 
+%Sets the domain of all vertices between 1 and the specified value R
 setDomain([], _).
 setDomain([L|Ls], R) :-
 		domain(L, 1, R),
 		setDomain(Ls, R).
 
+%Sets each horizontal line of the board to have distinct elements within that line
 horizontal([]).
 horizontal([L|Lr]) :-
 		all_distinct(L),
 		horizontal(Lr).
 
-diagonalRight(V) :- diagonalRight(V, 1).
-diagonalRight([], _).
-diagonalRight([L|Ls], N) :- fail.
+%Sets each "\" diagonal of the board to have distinct elements within that diagonal
+diagonalsRight(V) :-
+        length(V, N),
+        diagonalsRight(V, N).
+diagonalsRight(_, 0).
+diagonalsRight(V, N) :-
+        restrictRight(V, N),
+        M is N - 1,
+        diagonalsRight(V, M).
 
-diagonalLeft(V) :- diagonalLeft(V, 1).
-diagonalLeft([], _).
-diagonalLeft([L|Ls], N) :- fail.
+restrictRight(V, N).
 
+%Sets each "/" diagonal of the board to have distinct elements within that diagonal
+diagonalsLeft(V) :-
+        length(V, N),
+        diagonalsLeft(V, N).
+diagonalsLeft(_, 0).
+diagonalsLeft(V, N) :-
+        restrictLeft(V, N),
+        M is N - 1,
+        diagonalsLeft(V, M).
+
+restrictLeft(V, N) :- restrictLeft(V, N, []).
+restrictLeft([], _, Diagonal) :- all_distinct(Diagonal).
+restrictLeft([X|Xs], N, Acc) :-
+        element(N, X, Elem),
+        append(Acc, [Elem], App),
+        restrictLeft(Xs, N, App).
+restrictLeft([_|Xs], N, Acc) :-
+        restrictLeft(Xs, N, Acc).
+
+%imposes the restrictions regarding the sum of three vertices
 innerSums([]).
 innerSums([X|Xs]) :-
         innerSum(X),
         innerSums(Xs).
-
 innerSum([T1, T2, T3, Sum]) :-
         T1 + T2 + T3 #= Sum.
 
+%flattens the lines of vertices list into a list of vertices
 flatten(List, Flat) :- flatten(List, Flat, []).
 flatten([], Flat, Flat).
 flatten([X|Xs], Flat, Acc) :-
